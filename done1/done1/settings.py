c2b69bd790f59c7d97855a384b7da71956314da2
JovinMtl <nsanzumukizath@gmail.com>
2024-03-19 16:35:13 +0200

    changing the token lifetime into 5minutes

--- done1/done1/settings.py
+++ done1/done1/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("JWT",),
     "AUTH_HEADER_TYPES": ("Bearer",),
