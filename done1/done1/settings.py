2b9b1049d3b7dafde3de4baed94af3fe7a37eb8b
JovinMtl <nsanzumukizath@gmail.com>
2024-03-23 11:27:52 +0200

    adding MEDIA_URL for stating the images being served

--- done1/done1/settings.py
+++ done1/done1/settings.py
@@ -137,8 +137,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_URL = "/home/muteule/Coding/Py_enjoy/Learn_django/static/"
 
 # Media settings example
-MEDIA_URL = 'depots/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'depots/')
+MEDIA_URL = '/prof/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'depots')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
