98a96dd70351fe4d759077eb9dd3d606c4a00ec9
JovinMtl <nsanzumukizath@gmail.com>
2024-04-06 12:24:37 +0200

    setting SMTP Email Configuration

--- done1/done1/settings.py
+++ done1/done1/settings.py
@@ -176,6 +176,14 @@ SIMPLE_JWT = {
 # SECURE_SSL_REDIRECT = True
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+# SMTP Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'jovintry@gmail.com'
+EMAIL_HOST_PASSWORD = 'Tijovin123456'
+
 
 
 
